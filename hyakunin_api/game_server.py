648f888835a44515d5397c078c6b669ac4040c38
Akira Tanaka <akira.tanaka.1897@gmail.com>
2018-06-07 22:24:18 +0900

    add game_server.py / refactor code of web_server.py

--- hyakunin_api/game_server.py
+++ hyakunin_api/game_server.py
@@ -1,31 +1,40 @@
 from bottle import route, run, static_file, request, template
 import requests
 
-# In this template the web server returns search result.
-TEMPLATE_RESULT_HTML ='''
+# Answer Page Template
+QUIZ_HTML = '''
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
         "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <title>Result - Hyakunin-Isshu Search</title>
+    <meta charset="UTF-8">
+    <title>Hyakunin-Isshu Game</title>
+    <script type="text/javascript">
+    <!--
+        function print_answer(){
+            window.alert('{{second_p}}')
+        }
+    -->
+    </script>
 </head>
 <body>
-<h1>Result - Hyakunin-Isshu Search</h1>
-<table rules="all">
-    <tr>
-        <th>ID</th><th>Writer</th><th>1st phrase</th><th>2nd phrase</th>
-    </tr>
-    <tr>
-        <td>{{id}}</td><td>{{writer}}</td><td>{{first_p}}</td><td>{{second_p}}</td>
-    </tr>
-</table>
-<a href="../">Back to home</a>
+    <h1>Hyakunin-Isshu Quiz</h1>
+    <h2>First Phrase</h2>
+    <p>{{first_p}}</p>
+    <p>
+        <input type="button" value="Show Second Phrase"
+               onclick="print_answer()">
+    </p>
+    <p>
+        <a href="./quiz?id={{next_id}}">Next</a>
+    </p>
+    <p>
+        <a href="../">Return to Home</a>
+    </p>
 </body>
 </html>
 '''
 
-# The web server returns error page if the song is not found
-#       (i.e. the API Server returns 404)
 NOT_FOUND_HTML = '''
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
         "http://www.w3.org/TR/html4/loose.dtd">
@@ -42,7 +51,6 @@ NOT_FOUND_HTML = '''
 '''
 
 
-# Connect to the API server and fetch song data.
 def fetch_result(id):
     url = 'http://localhost:10080/song/{id}'.format(id=id)
     res = requests.get(url)
@@ -51,22 +59,19 @@ def fetch_result(id):
     values = res.text.split('|')
     return {
         "id": values[0],
-        "writer": values[1],
         "first_p": values[3],
         "second_p": values[4]
     }
 
 
-# https://bottlepy.org/docs/dev/tutorial.html#generating-content
-# Show search box
 @route('/')
 def search_box():
-    return static_file('searchbox.html', root='html/')
+    return static_file('question.html', root='html/')
 
 
 # Run search
-@route('/search')
-def show_result():
+@route('/quiz')
+def show_quiz():
     # Parse GET request and get song ID
     id = request.query.id
     # Connect to API server
@@ -74,13 +79,12 @@ def show_result():
     if not res_dict:
         return NOT_FOUND_HTML
     return template(
-        TEMPLATE_RESULT_HTML,
-        id=res_dict['id'],
-        writer=res_dict['writer'],
+        QUIZ_HTML,
+        next_id=int(res_dict['id'])%100+1,
         first_p=res_dict['first_p'],
         second_p=res_dict['second_p']
     )
 
 
-# Run the web server at http://localhost:8080
-run(host='localhost', port=8080)
+# Run the web server at http://localhost:8081
+run(host='localhost', port=8081)
